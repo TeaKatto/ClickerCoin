61f75e0e32946bf3abb69b5788ee680ab82b991b
eyals208 <eyalsrugo@gmail.com>
2022-12-19 19:44:59 +0200

    added particle effect on coin upgrade

--- Assets/Scripts/Updragers/CoinUpgrader.cs
+++ Assets/Scripts/Updragers/CoinUpgrader.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 
 public class CoinUpgrader : AbstractUpgrader
 {
+    ParticleSystem particleSystem;
     CompleteRotationDetector rotationDetector;
     
     // Start is called before the first frame update
     void Start()
     {
         rotationDetector = FindObjectOfType<CompleteRotationDetector>();
+        particleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     protected override void Upgrade(Upgrade upgrade)
     {
         rotationDetector.RotationValue = upgrade.value; // increase value multimlier for each rotation
-        ReplaceToNewGameObject(upgrade.visualUpgrade);   
+        ReplaceToNewGameObject(upgrade.visualUpgrade);
+        particleSystem.Play();
     }
 
 }
